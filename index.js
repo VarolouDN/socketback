7b01023e8045a226c01a18056b6134f9b853a7cb
Denys Novosad <denisnovosad3@gmail.com>
2023-11-15 08:48:27 +0200

    first commit back

---
+++ index.js
@@ -0,0 +1,30 @@
+//const cors=require('cors')
+const io = require('socket.io')({
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['Content-Type'],
+    },
+});
+
+io.listen(5000);
+const users={}
+
+io.on("connection",socket=>{
+     console.log("new User")
+   // socket.emit("chat-message","Hi There")
+    socket.on('new-user',name=>{
+       users[socket.id] =name
+        socket.broadcast.emit('user-connected',name)
+    })
+    socket.on('send-chat-message',message=>{
+        console.log(message)
+        socket.broadcast.emit('chat-message',{message:message,name:users[socket.id],id:socket.id})
+    })
+    socket.on('disconnect',()=>{
+
+        socket.broadcast.emit('user-disconnected',users[socket.id])
+        delete users[socket.id]
+    })
+
+})
