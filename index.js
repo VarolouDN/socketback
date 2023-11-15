6cf79e913222f24549c4648e61ad15ce8db4469b
Denys Novosad <denisnovosad3@gmail.com>
2023-11-15 16:56:04 +0200

    15 back

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const io = socketIo(server);
 });*/
 //app.use(cors(corsParams))
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://socketfront.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
